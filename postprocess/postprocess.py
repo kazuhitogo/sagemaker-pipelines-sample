bac533f958f591ef53354a5f10b028f95e7afcb2
kazuhitogo <kazuhitogo@gmail.com>
2021-01-22 01:28:40 +0000

    initial commit(not work yet)

---
+++ postprocess/postprocess.py
@@ -0,0 +1,49 @@
+import json, pickle, tarfile, os, argparse
+import numpy as np
+import xgboost
+
+from sklearn.metrics import roc_auc_score
+
+def arg_parse():
+    parser = argparse.ArgumentParser()
+    print(parser)
+    parser.add_argument('--input-dir', type=str, default=None)
+    parser.add_argument('--output-dir', type=str, default=None)
+    parser.add_argument('--model-dir', type=str, default=None)
+    args, _ = parser.parse_known_args()
+    print(f'Received arguments {args}')
+    return args
+
+
+if __name__ == "__main__":
+    args = arg_parse()
+    model_path = os.path.join(args.model_dir, 'model.tar.gz')
+    with tarfile.open(model_path) as tar:
+        tar.extractall(path=".")
+    
+    model = pickle.load(open("xgboost-model", "rb"))
+
+    test_csv_path = os.path.join(args.input_dir,'test.csv')
+    test = np.loadtxt(test_csv_path, delimiter=',')
+    test_x = xgboost.DMatrix(test[:,1:])
+    pred_y = model.predict(test_x)
+    print(pred_y)
+    answer_y = test[:,0]
+    
+    
+    auc = roc_auc_score(answer_y,pred_y)
+    
+    print(f'auc: {auc}')
+    
+    report_dict = {
+        "classification_metrics": {
+            "auc": {
+                "value": auc,
+            },
+        },
+    }
+
+    eval_result_path = os.path.join(args.output_dir, 'evaluation.json')
+    with open(eval_result_path, "w") as f:
+        f.write(json.dumps(report_dict))
+    exit()
